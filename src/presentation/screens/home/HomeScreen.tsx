fc746d5fd8b4db09ec41b57bc2d55dc59b979743
Reinaldo Contreras <reinaldojcg1@gmail.com>
2024-04-24 19:20:08 -0500

    Add HorizontalCarousel component

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -4,11 +4,12 @@ import {useMovies} from '../../hooks/useMovies';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {ScrollView} from 'react-native-gesture-handler';
 import {PosterCarousel} from '../../components/movies/PosterCarousel';
+import {HorizontalCarousel} from '../../components/movies/HorizontalCarousel';
 
 export const HomeScreen = () => {
   const {top} = useSafeAreaInsets();
 
-  const {isLoading, nowPlaying} = useMovies();
+  const {isLoading, nowPlaying, popular, topRated, upcoming} = useMovies();
 
   if (isLoading) {
     return <Text>Cargando...</Text>;
@@ -18,6 +19,15 @@ export const HomeScreen = () => {
     <ScrollView>
       <View style={{marginTop: top + 20, paddingBottom: 30}}>
         <PosterCarousel movies={nowPlaying} />
+
+        {/* Populares */}
+        <HorizontalCarousel movies={popular} title="Populares" />
+
+        {/* Top Rated */}
+        <HorizontalCarousel movies={topRated} title="Mejor calificadas" />
+
+        {/* Próximamente */}
+        <HorizontalCarousel movies={upcoming} title="Próximamente" />
       </View>
     </ScrollView>
   );
